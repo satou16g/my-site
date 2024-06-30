8e00125cf7ce91830f2f11dc80d697f0b1a7d7bd
satou16g <satou16g0717@gmail.com>
2024-06-30 17:29:56 +0900

    aaa

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,7 +20,8 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="inline-block text-sm font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-block mb-2 text-sm font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        class="inline-block"
       >
         {secHeading ? (
           <h2 {...headerProps}>{title}</h2>
