1dfc9d2791ebacb27682216f38c8690fc5e6d20d
satou16g <satou16g0717@gmail.com>
2024-06-30 10:21:47 +0900

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        sans: ["BIZ UDGothic", "sans-serif"],
+        "BIZ UDGothic", "sans-serif",
       },
 
       typography: {
