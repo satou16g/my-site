11d3e203cbf74c9e431a7d9fe738e9c36f240bda
satou16g <satou16g0717@gmail.com>
2024-06-30 10:57:12 +0900

    a

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -26,9 +26,17 @@ export default function Datetime({
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
       </svg>
+      { pubDatetime ? (
+        <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
+          Updated:
+        </span>
+      ) : (
+        <span className="sr-only">Published:</span>
+        </span>
+      )}
       <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
         <FormattedDatetime
-          pubDatetime={ pubDatetime }
+          pubDatetime={pubDatetime}
         />
       </span>
     </div>
@@ -36,7 +44,7 @@ export default function Datetime({
 }
 
 const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
-  const myDatetime = new Date( pubDatetime ? pubDatetime
+  const myDatetime = new Date( pubDatetime
   );
 
   const date = myDatetime.toLocaleDateString(LOCALE.langTag, {
