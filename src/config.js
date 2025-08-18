b16f1139a568f442d78de2a43b6220b06d1ed206
satou16g <satou16g0717@gmail.com>
2025-08-18 14:38:35 +0900

    config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,8 @@ export const PandaConfig = {
     defaultLocale: 'en',
     navbar: [
         // {title: 'Posts', url: '/'}, // auto generated
-        { title: 'about', url: '/about/' }
+        { title: 'about', url: '/about/' },
+        { title: 'contact', url: 'https://forms.gle/dEkYCg1qyata5EHA8'}
     ],
     footer: [
         {  }
